9a1a7718350042646b769d04bf4966225a0e344b
Alexandre Bekor <staff@agenciabekor.com>
2023-02-14 15:16:10 -0300

    :rocket: added context to auth

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,7 +1,12 @@
-import { createContext } from "react";
+import { signInRequest } from "@/services/auth";
+import Router from "next/router";
+import { setCookie } from "nookies";
+import { createContext, useState } from "react";
 
 type AuthContextProps = {
+  user: UserProps | null;
   isAuthenticated: boolean;
+  signIn: (data: SignInData) => void;
 };
 
 export const AuthContext = createContext({} as AuthContextProps);
@@ -10,11 +15,38 @@ type AuthProviderProps = {
   children: React.ReactNode;
 };
 
+type SignInData = {
+  email: string;
+  password: string;
+};
+
+type UserProps = {
+  name: string;
+  email: string;
+  avatar: string;
+};
+
 export const AuthProvider = ({ children }: AuthProviderProps) => {
-  const isAuthenticated = false;
+  const [user, setUser] = useState<UserProps | null>(null);
+  const isAuthenticated = !!user;
+
+  const signIn = async ({ email, password }: SignInData) => {
+    const { token, user } = await signInRequest({
+      email,
+      password,
+    });
+
+    setCookie(undefined, "poc-token", token, {
+      maxAge: 60 * 60 * 1, // 1 hour
+    });
+
+    setUser(user);
+
+    Router.push("/dashboard");
+  };
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated }}>
+    <AuthContext.Provider value={{ user, isAuthenticated, signIn }}>
       {children}
     </AuthContext.Provider>
   );
